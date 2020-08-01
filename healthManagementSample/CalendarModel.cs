81267087517c3fcc152b7a93dc8e1cebf5bc594d
abdur <abdur.mohammad@xam-consulting.com>
2020-08-01 12:14:36 +1000

    Done

---
+++ healthManagementSample/CalendarModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace healthManagementSample
+{
+    public class CalendarModel
+    {
+        public string Weekday { get; set; }
+        public string MonthDay { get; set; }
+    }
+}
---
+++ healthManagementSample/ReportViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace healthManagementSample
+{
+    public class ReportViewModel
+    {
+
+        public ObservableCollection<CalendarModel> CalendarList { get; set; } = new ObservableCollection<CalendarModel>();
+
+        public ReportViewModel()
+        {
+
+            CalendarList.Add(new CalendarModel { Weekday = "Fri", MonthDay = "14" });
+            CalendarList.Add(new CalendarModel { Weekday = "Sat", MonthDay = "15" });
+            CalendarList.Add(new CalendarModel { Weekday = "Sun", MonthDay = "16" });
+            CalendarList.Add(new CalendarModel { Weekday = "Mon", MonthDay = "17" });
+            CalendarList.Add(new CalendarModel { Weekday = "Tue", MonthDay = "18" });
+            CalendarList.Add(new CalendarModel { Weekday = "Wed", MonthDay = "19" });
+            CalendarList.Add(new CalendarModel { Weekday = "Thu", MonthDay = "20" });
+            CalendarList.Add(new CalendarModel { Weekday = "Fri", MonthDay = "21" });
+            CalendarList.Add(new CalendarModel { Weekday = "Sat", MonthDay = "22" });
+            CalendarList.Add(new CalendarModel { Weekday = "Fri", MonthDay = "14" });
+            CalendarList.Add(new CalendarModel { Weekday = "Sat", MonthDay = "15" });
+            CalendarList.Add(new CalendarModel { Weekday = "Sun", MonthDay = "16" });
+            CalendarList.Add(new CalendarModel { Weekday = "Mon", MonthDay = "17" });
+            CalendarList.Add(new CalendarModel { Weekday = "Tue", MonthDay = "18" });
+            CalendarList.Add(new CalendarModel { Weekday = "Wed", MonthDay = "19" });
+            CalendarList.Add(new CalendarModel { Weekday = "Thu", MonthDay = "20" });
+            CalendarList.Add(new CalendarModel { Weekday = "Fri", MonthDay = "21" });
+            CalendarList.Add(new CalendarModel { Weekday = "Sat", MonthDay = "22" });
+
+        }
+    }
+}
